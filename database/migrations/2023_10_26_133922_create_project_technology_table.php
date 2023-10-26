03d3bcd99d05afe8627e3e71ab4a518b655c22f0
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-26 15:47:53 +0200

    feat: add constraint & Foreign key to pivot table.

--- database/migrations/2023_10_26_133922_create_project_technology_table.php
+++ database/migrations/2023_10_26_133922_create_project_technology_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,7 +14,18 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            /* $table->timestamps(); 
+             * Pivot table can't be null because the db struggling and relations get broke. It's better to use
+             * cascadeOnDelete - >  Questo cancella solo le relazioni tra le tabelle.
+             */
+
+            $table->foreignId('project_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('technology_id')
+                ->constrained()
+                ->cascadeOnDelete();
         });
     }
 
