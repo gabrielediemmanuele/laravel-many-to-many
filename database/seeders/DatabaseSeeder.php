bc5264045ac5f04315b80445be497d73f38fbf3d
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-26 16:09:13 +0200

    feat: add TechnologySeeder to Db.seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             TypeSeeder::class,
+            TechnologySeeder::class,
             ProjectSeeder::class,
         ]);
     }
