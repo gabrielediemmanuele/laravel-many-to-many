5a2cad70cff8dc4d6ea31d494aa8b84d2a18896c
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-26 16:07:58 +0200

    feat: add tec. array to Tech.seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* dentro il seeder creo l'array di nomi che mi servono  */
+        $tec_names = ["HTML", "CSS", "JavaScript", "SQL", "PHP", "VUE", "VITE", "BLADE", "GIT"];
+
+        /*  con un foreach genero tanti nuove "tecnologie per quante sono dentro tec_names"  */
+        foreach ($tec_names as $tec_name) {
+
+            $technology = new Technology();
+
+            $technology->tech_name = $tec_name;
+
+            $technology->save();
+        }
+    }
+}
